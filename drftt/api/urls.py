b5763991b24d7f74eed80d8ef495ed05f057acf1
bervr <bervrr@gmail.com>
2022-06-23 09:57:48 +0400

    .env keys

--- drftt/api/urls.py
+++ drftt/api/urls.py
@@ -21,6 +21,7 @@ from rest_framework import routers
 router = routers.DefaultRouter()
 router.register(r'level', views.GameLevelViewSet)
 
+app_name = 'api'
 
 urlpatterns = [
     path('', include(router.urls)),
--- drftt/drftt/urls.py
+++ drftt/drftt/urls.py
@@ -19,7 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('api.urls')),
+    path('api/', include('api.urls', namespace='api')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
 
