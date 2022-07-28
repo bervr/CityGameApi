b6b514e366b983426881d6c378a025971fe9ba13
bervr <bervrr@gmail.com>
2022-07-28 11:42:04 +0400

    fix settings after rename

--- city_game_api/api/tests.py
+++ city_game_api/api/tests.py
@@ -1,3 +1,11 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+# from city_game_api.api.models import Game
 
-# Create your tests here.
+
+class GameTests(APITestCase):
+    def test_start_game(self):
+        url = reverse('game')
+        response = self.client.get(url, format('json'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
