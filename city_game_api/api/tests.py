91bdba8ce6aca2e73f5bd24890b29b21e9bf4caa
bervr <bervrr@gmail.com>
2022-08-08 18:44:42 +0400

    factory boy trying

--- city_game_api/api/tests.py
+++ city_game_api/api/tests.py
@@ -5,6 +5,24 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase
 from api.models import Game, GameLevel
+import factory
+
+
+class GameFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Game
+    game_name = factory.Faker("name")
+    game_start = factory.LazyFunction(datetime.datetime.now)
+    game_finish = factory.LazyFunction(datetime.datetime.now)
+
+
+class GameLevelFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = GameLevel
+
+    number = factory.Sequence(lambda n: f"{n}")
+    level_of_game = factory.SubFactory(GameFactory)
+
 
 
 class GameTests(APITestCase):
@@ -13,25 +31,26 @@ class GameTests(APITestCase):
         user_test1.save()
         user_test2 = User.objects.create_user(username='test2', password='4asdfgH56')
         user_test2.save()
-
-        self.one_game = Game.objects.create(
-            game_number=1,
-            game_name='first_game',
-            game_start=datetime.datetime.now(),
-            game_finish=datetime.datetime.now() + datetime.timedelta(hours=5)
-        )
-        self.one_level = GameLevel.objects.create(
-            level_of_game=self.one_game,
-            number=1,
-            geo_lat=123456.12,
-            geo_lng=987654.98,
-            name='first_level',
-            task='first_task',
-            answer='1111',
-            promt1='1',
-            promt2='11',
-            promt3='111'
-        )
+        self.one_game = GameFactory()
+        # self.one_game = Game.objects.create(
+        #     game_number=1,
+        #     game_name='first_game',
+        #     game_start=datetime.datetime.now(),
+        #     game_finish=datetime.datetime.now() + datetime.timedelta(hours=5)
+        # )
+        # self.one_level = GameLevelFactory()
+        # self.one_level = GameLevel.objects.create(
+        #     level_of_game=self.one_game,
+        #     number=1,
+        #     geo_lat=123456.12,
+        #     geo_lng=987654.98,
+        #     name='first_level',
+        #     task='first_task',
+        #     answer='1111',
+        #     promt1='1',
+        #     promt2='11',
+        #     promt3='111'
+        # )
 
     # def test_post_answer(self):
     #     url = reverse('api:post_answer')
@@ -47,13 +66,13 @@ class GameTests(APITestCase):
         response = self.client.get(url, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_level_exists(self):
-        url = reverse('api:level', kwargs={"game": self.one_game.game_number, "pk": self.one_level.number})
-        response = self.client.get(url, format='json')
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
-
-    def test_level_constrait(self):
-        url = reverse('api:level', kwargs={"game": self.one_game.game_number, "pk": 18})
-        response = self.client.get(url, format='json')
-        print(response)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
+    # def test_level_exists(self):
+    #     url = reverse('api:level', kwargs={"game": self.one_game.game_number, "pk": self.one_level.number})
+    #     response = self.client.get(url, format='json')
+    #     self.assertEqual(response.status_code, status.HTTP_200_OK)
+    #
+    # def test_level_constrait(self):
+    #     url = reverse('api:level', kwargs={"game": self.one_game.game_number, "pk": 18})
+    #     response = self.client.get(url, format='json')
+    #     print(response)
+    #     # self.assertEqual(response.status_code, status.HTTP_200_OK)
